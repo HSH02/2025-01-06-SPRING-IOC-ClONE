a11157394fd249f4250f70c8a08e1fc11175a028
han <tkrhktkdwk10@gmail.com>
2025-01-06 11:17:21 +0900

    getParameters, b

--- src/test/java/util/ClsUtilTest.java
+++ src/test/java/util/ClsUtilTest.java
@@ -39,5 +39,17 @@ public class ClsUtilTest {
         assertThat(parameters[1].getType()).isEqualTo(int.class);
     }
 
+    @Test
+    @DisplayName("ClsUtil.getParameters with cls")
+    void t4() {
+        Parameter[] parameters = ClsUtil.getParameters(TestCar.class, new Object[]{"BMW", 1234});
+
+        assertThat(parameters[0].getName()).isEqualTo("name");
+        assertThat(parameters[0].getType()).isEqualTo(String.class);
+
+        assertThat(parameters[1].getName()).isEqualTo("number");
+        assertThat(parameters[1].getType()).isEqualTo(int.class);
+    }
+
 
 }
