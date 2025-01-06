5cb9f35f8461bd312a10e6be500f0f3a5fe79233
han <tkrhktkdwk10@gmail.com>
2025-01-06 09:39:16 +0900

    ApplicationContext class

---
+++ src/test/java/ApplicationContextTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationContextTest {
+    private static ApplicationContext applicationContext;
+
+    @BeforeAll
+    public static void beforeAll() {
+        applicationContext = new ApplicationContext("com");
+        applicationContext.init();
+    }
+
+    @Test
+    @DisplayName("ApplicationContext 객체 생성")
+    public void t1() {
+        System.out.println(applicationContext);
+    }
+}
