a71610711a2580b094104f712e71e2764151097c
han <tkrhktkdwk10@gmail.com>
2025-01-06 10:45:34 +0900

    ClsUtil.loadClass, g

---
+++ src/main/java/com/framework/ioc/util/ClsUtil.java
@@ -0,0 +1,11 @@
+package com.framework.ioc.util;
+
+import lombok.SneakyThrows;
+
+public class ClsUtil {
+
+    @SneakyThrows
+    public static <T> Class<T> loadClass(String clsPath) {
+        return (Class<T>) Class.forName(clsPath);
+    }
+}
--- src/test/java/util/ClsUtilTest.java
+++ src/test/java/util/ClsUtilTest.java
@@ -1,10 +1,6 @@
 package util;
 
-import com.ApplicationContext;
-import com.repository.TestPostRepository;
-import com.service.TestFacadePostService;
-import com.service.TestPostService;
-import org.junit.jupiter.api.BeforeAll;
+import com.framework.ioc.util.ClsUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.sample.TestCar;
@@ -16,8 +12,7 @@ public class ClsUtilTest {
     @Test
     @DisplayName("ClsUtil.loadClass")
     void t1() {
-        Class<TestCar> cls = ClsUtil.loadClass("com.framework.ioc.util.sample.TestCar");
-
+        Class<TestCar> cls = ClsUtil.loadClass("util.sample.TestCar");
         assertThat(cls).isEqualTo(TestCar.class);
     }
 
