48d591cf94ebf59e5907eed3aed20c38f9b5e03e
han <tkrhktkdwk10@gmail.com>
2025-01-06 11:04:29 +0900

    ClsUtil.construct,r

--- src/test/java/util/ClsUtilTest.java
+++ src/test/java/util/ClsUtilTest.java
@@ -16,5 +16,14 @@ public class ClsUtilTest {
         assertThat(cls).isEqualTo(TestCar.class);
     }
 
+    @Test
+    @DisplayName("ClsUtil.construct")
+    void t2() {
+        TestCar testCar = ClsUtil.construct("util.sample.TestCar", new Object[]{"BMW", 1234});
+
+        assertThat(testCar.getName()).isEqualTo("BMW");
+        assertThat(testCar.getNumber()).isEqualTo("1234");
+    }
+
 
 }
--- src/test/java/util/sample/TestCar.java
+++ src/test/java/util/sample/TestCar.java
@@ -1,8 +1,10 @@
 package util.sample;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class TestCar {
     private String name;
     private int number;
