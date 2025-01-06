7dd274f948070d33930635f05ddf7737d6f79412
han <tkrhktkdwk10@gmail.com>
2025-01-06 11:10:13 +0900

    ClsUtil.construct, b

--- src/test/java/util/ClsUtilTest.java
+++ src/test/java/util/ClsUtilTest.java
@@ -10,16 +10,16 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class ClsUtilTest {
 
     @Test
-    @DisplayName("ClsUtil.loadClass")
+    @DisplayName("ClsUtil.loadClass with classPath")
     void t1() {
         Class<TestCar> cls = ClsUtil.loadClass("util.sample.TestCar");
         assertThat(cls).isEqualTo(TestCar.class);
     }
 
     @Test
-    @DisplayName("ClsUtil.construct")
+    @DisplayName("ClsUtil.construct with cls")
     void t2() {
-        TestCar testCar = ClsUtil.construct("util.sample.TestCar", new Object[]{"BMW", 1234});
+        TestCar testCar = ClsUtil.construct(TestCar.class, new Object[]{"BMW", 1234});
 
         assertThat(testCar.getName()).isEqualTo("BMW");
         assertThat(testCar.getNumber()).isEqualTo(1234);
