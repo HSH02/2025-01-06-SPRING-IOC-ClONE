d9567961ae577aa4425be4ff89936e6706c9061a
han <tkrhktkdwk10@gmail.com>
2025-01-06 10:28:46 +0900

    testFacadePostService has testPostService, testPostRepository, r

--- src/main/java/com/service/TestPostService.java
+++ src/main/java/com/service/TestPostService.java
@@ -1,7 +1,11 @@
 package com.service;
 
 import com.framework.annotations.Service.Service;
+import com.repository.TestPostRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class TestPostService {
+    private final TestPostRepository testPostRepository;
 }
